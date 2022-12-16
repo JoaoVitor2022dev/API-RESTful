b5c851e26884580d46beb745058d683bb6640cb2
Joao Vitor Mocambite Lima <86741407+JoaoVitor2022dev@users.noreply.github.com>
2022-12-16 17:00:31 -0400

    Add files via upload

---
+++ API rest FUL/index.js
@@ -0,0 +1,41 @@
+// configuraçao inicial 
+
+const express = require('express'); 
+const app =  express(); 
+const mongoose = require('mongoose');
+
+const Person = require('./models/Person');
+
+// forma de ler JSON / middlewares
+
+app.use( 
+    express.urlencoded({
+        extended: true, 
+    }), 
+) 
+
+app.use(express.json()) 
+
+// rota inicial / endPoint 
+app.get('/', (requisition, reposta) => {
+   // mostrar requisition 
+   
+   // mostrar reposta 
+   reposta.json({menssage: 'ola exprexx'}); 
+
+});
+
+const DB_USER = 'Vitor'; 
+const DB_PASSWORD = encodeURIComponent('H4eT.kP2Rb4x@Hq'); 
+
+mongoose.connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.l3zpv5i.mongodb.net/bancodaapi?retryWrites=true&w=majority`
+    ).then( () => {
+        console.log('conectamos com o mongoDB');
+        app.listen(4000); 
+    }).catch( (err) => { console.log(err);})
+
+
+
+
+
