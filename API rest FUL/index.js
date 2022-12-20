7027c571486a5eedd3fb955bfd67fb38532d3059
Joao Vitor Mocambite Lima <86741407+JoaoVitor2022dev@users.noreply.github.com>
2022-12-20 17:18:10 -0400

    Update index.js

--- API rest FUL/index.js
+++ API rest FUL/index.js
@@ -16,6 +16,43 @@ app.use(
 
 app.use(express.json()) 
 
+
+// rotas da API 
+app.post('/person', async ( requisition , response ) => {
+   // requisition.body  
+
+    // {name: 'vitor lima', salary: 3000 , approved: true } 
+    const {name, salary , approved} = requisition.body; 
+
+    // verificaçao de name 
+
+    if (!name) {
+        response.status(422).json({error: 'O nome é obrigatorio...'})
+    }
+    if (!salary) {
+        response.status(422).json({error: 'O salario é obrigatorio...'})
+    } 
+ 
+    const person = {
+        name, 
+        salary, 
+        approved,
+    }
+
+     try {        
+        // criando dados 
+        await Person.create(person); 
+
+        response.status(201).json({message: 'Pessoa inserindo no sistema com sucesso!'}); 
+         console.log('inserindo dados com sucesso!');
+
+     } catch (error) {
+        response.status(500).json({error: error})
+     }
+     
+}); 
+
+
 // rota inicial / endPoint 
 app.get('/', (requisition, reposta) => {
    // mostrar requisition 
@@ -25,15 +62,19 @@ app.get('/', (requisition, reposta) => {
 
 });
 
-const DB_USER = 'userNULL'; 
-const DB_PASSWORD = encodeURIComponent('bkjhguygo2326hkhjg'); 
+// conection com mogoose
 
+const DB_USER = 'ehfbwjhebfjh'; 
+const DB_PASSWORD = encodeURIComponent('fqiweho fiuqehw'); 
+
+mongoose.set('strictQuery', true); 
 mongoose.connect(
     `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.l3zpv5i.mongodb.net/bancodaapi?retryWrites=true&w=majority`
     ).then( () => {
         console.log('conectamos com o mongoDB');
         app.listen(4000); 
     }).catch( (err) => { console.log(err);})
+
 
 
 
